4dcaf8a7a433fcb7c8271a4805cf73e9fe2d1ef7
Zhao Hu <huzhao1991@gmail.com>
2017-09-27 16:35:54 -0400

    data prepare

--- source/train_zhao2.py
+++ source/train_zhao2.py
@@ -8,6 +8,16 @@ import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+
+def cnn_model_fn(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    input_layer = tf.reshape(features['x'],[-1, 180, 180, 3])
+
+    # Conv 1
+    conv1 = tf.layers.conv2d()
+
+
 # Our application logic will be added here
 
 if __name__ == "__main__":
